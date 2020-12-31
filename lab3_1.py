0659cb118555fbd426826ca33c8e021d3453958a
natalia <choum_natusia@gmail.com>
2020-12-31 12:24:11 +0300

    #работает

--- lab3_1.py
+++ lab3_1.py
@@ -1,19 +1,10 @@
 with open("D:\programming.txt", "r") as file:
    # print(file.read())
    for line in file:
-        i=0
-        t=0
-        #print(len(line))
-        while line[i]==" ":
-           i += 1
-        n = i
-        #if len(line)== 2:
-           #k=0
-        #else:
-        for p in range(len(line)-i, len(line)):
-            t += 1
-            if line[p]==" ":
-                #print(t)
-                 break
-        k = len(line)-n-t
-        print(line[n:len(line)-k])
+       i=0
+       while line[i]==" ":
+           line = line[1:]
+       while line[len(line)-1]==" ":
+           line = line[:-1]
+       print(line)
+
