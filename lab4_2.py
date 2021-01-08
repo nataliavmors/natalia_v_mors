b202e1efcbfea5d6385b115426fc789166f95b36
natalia <choum_natusia@gmail.com>
2021-01-08 17:34:50 +0300

    # работает

--- lab4_2.py
+++ lab4_2.py
@@ -1,22 +1,23 @@
 def decorator(function):
-    def wrapper():
-        obj = function(nums)
-        if len(obj) > 10:
-            return "очень много"
-        elif len(obj) == 0:
-            return "нет("
+    def wrapper(a):
+        kkk=function(a)
+        if kkk > 10:
+            print("очень много")
+        elif kkk == 0:
+            print("нет(")
         else:
-            return obj
+            print(a)
     return wrapper
 
-
+@decorator
 def even_num(nums):
     obj = []
     for i in nums:
         if i % 2 == 0:
             obj.append(i)
-    return obj
+    a=len(obj)
+    return a
 
 
 nums = list(map(int, input().split()))
-print(decorator(even_num(nums)))
+decorator(even_num(nums))
